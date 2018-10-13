5f387af50cbcaa66bef17d4cf2cd63d6e24c48cc
scubamut@gmail.com <scubamut@gmail.com>
2018-10-13 15:18:27 +0200

    setup.py version bumped to 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='fintools',
-    version='0.1.2',
+    version='0.1.3',
     author='Dave Gilbert',
     author_email='scubamut @gmail.com',
     packages=['fintools'],
