04587c79bb3a5b02756f1fe1cfc6d00ab44ca1bc
scubamut <scubamut@gmail.com>
2019-05-08 11:05:52 +0200

    updated 00

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='fintools',
-    version='0.2.4',
+    version='0.2.5',
     author='Dave Gilbert',
     author_email='scubamut @gmail.com',
     packages=['fintools'],
