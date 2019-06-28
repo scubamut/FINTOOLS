95b1301d4d9f550acf2448ca94093c24709680d8
scubamut <scubamut@gmail.com>
2019-06-28 12:35:43 +0200

    1. 'close' should have been 'adj close' 2. Bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='fintools',
-    version='0.2.5',
+    version='0.2.6',
     author='Dave Gilbert',
     author_email='scubamut @gmail.com',
     packages=['fintools'],
