632eef9d04dc416181d250adbc3704cac0763630
scubamut <scubamut@mail.com>
2021-02-12 14:04:55 +0000

    1. update __init__.py 2. Bumped version to 0.5.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='fintools',
-    version='0.4.0',
+    version='0.5.0',
     author='Dave Gilbert',
     author_email='scubamut @gmail.com',
     packages=['fintools'],
