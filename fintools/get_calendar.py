78dda378f7fc1c53554d793812fa37234a92eb93
scubamut <scubamut@gmail.com>
2024-08-18 17:28:17 +0100

    changes for calendar

--- fintools/get_calendar.py
+++ fintools/get_calendar.py
@@ -1,2 +1,5 @@
-import pandas_market_calendars as mcal
-nyse = mcal.get_calendar('NYSE')
+def get_calendar(calendar_name='NYSE'):
+
+    import pandas_market_calendars as mcal
+
+    return mcal.get_calendar(calendar_name)
